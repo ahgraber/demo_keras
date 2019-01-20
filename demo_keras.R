9f2625274e13c5721ba80e6d40430bcb3c69315f
Alex Graber <ahgraber@gmail.com>
2019-01-20 13:45:26 -0500

    offline updates

--- demo_keras.R
+++ demo_keras.R
@@ -1,45 +1,73 @@
 # install.packages('tensorflow')
 # install.packages('tfestimators')
 # install.packages('keras')
+# install.packages('reticulate')
 library(tensorflow)
 library(tfestimators)
 library(keras)
+library(reticulate)
+
+# to make virtual environment available to R, create symbolic link to ~/.virtualenv/VENV_NAME
+use_virtualenv("plaidml")
 use_backend("plaidml")
 
 # already installed on python side
 # install_tensorflow()
 # install_keras()
 
+batch_size <- 128
+num_classes <- 10
+epochs <- 12
+
+# Input image dimensions
+img_rows <- 28
+img_cols <- 28
+
 datasets <- tf$contrib$learn$datasets
-mnist <- datasets$mnist$read_data_sets("MNIST-data", one_hot = TRUE)
 mnist <- dataset_mnist(path='mnist.npz')
-x_train <- mnist$train$x
-y_train <- mnist$train$y
-x_test <- mnist$test$x
-y_test <- mnist$test$y
+# x_train <- mnist$train$x
+# y_train <- mnist$train$y
+# x_test <- mnist$test$x
+# y_test <- mnist$test$y
 
 # The x data is a 3-d array (images,width,height) of grayscale values . To
 # prepare the data for training we convert the 3-d arrays into matrices by
 # reshaping width and height into a single dimension (28x28 images are flattened
 # into length 784 vectors). Then, we convert the grayscale values from integers
 # ranging between 0 to 255 into floating point values ranging between 0 and 1:
-x_train <- array_reshape(x_train, c(nrow(x_train), 784))
-x_test <- array_reshape(x_test, c(nrow(x_test), 784))
-# rescale
+# Redefine  dimension of train/test inputs
+
+x_train <- array_reshape(mnist$train$x, c(nrow(mnist$train$x), img_rows, img_cols, 1))
+x_test <- array_reshape(mnist$test$x, c(nrow(mnist$test$x), img_rows, img_cols, 1))
+input_shape <- c(img_rows, img_cols, 1)
+# or
+x_train <- array_reshape(mnist$train$x, c(nrow(mnist$train$x), img_rows*img_cols*1))
+x_test <- array_reshape(mnist$test$x, c(nrow(mnist$test$x), img_rows*img_cols*1))
+input_shape <- c(img_rows, img_cols, 1)
+
+# Transform RGB values into [0,1] range
 x_train <- x_train / 255
 x_test <- x_test / 255
 
-y_train <- to_categorical(y_train, 10)
-y_test <- to_categorical(y_test, 10)
+cat('x_train_shape:', dim(x_train), '\n')
+cat(nrow(x_train), 'train samples\n')
+cat(nrow(x_test), 'test samples\n')
+
+# Convert class vectors to binary class matrices
+y_train <- to_categorical(mnist$train$y, num_classes)
+y_test <- to_categorical(mnist$test$y, num_classes)
 
 # define the model
-model <- keras_model_sequential()
-model %>%
-  layer_dense(units = 256, activation = 'relu', input_shape = c(784)) %>%
-  layer_dropout(rate = 0.4) %>%
-  layer_dense(units = 128, activation = 'relu') %>%
-  layer_dropout(rate = 0.3) %>%
-  layer_dense(units = 10, activation = 'softmax')
+model <- keras_model_sequential() %>%
+  layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = 'relu', input_shape = input_shape) %>% 
+  layer_conv_2d(filters = 64, kernel_size = c(3,3), activation = 'relu') %>% 
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>% 
+  layer_dropout(rate = 0.25) %>% 
+  layer_flatten() %>% 
+  layer_dense(units = 128, activation = 'relu') %>% 
+  layer_dropout(rate = 0.5) %>% 
+  layer_dense(units = num_classes, activation = 'softmax')
+
 
 summary(model)
 
@@ -51,12 +79,13 @@ model %>% compile(
 )
 
 # fit the model
-# 30 epochs
-# batches of 128 images
 history <- model %>% fit(
   x_train, y_train,
-  epochs = 30, batch_size = 128,
+  batch_size = batch_size,
+  epochs = epochs, 
+  verbose = T, 
   validation_split = 0.2
+  #validation_data=c(x_test, y_test)
 )
 
 # history object returned by fit() includes loss and accuracy metrics
